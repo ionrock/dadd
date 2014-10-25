bb8e65f1313db220a56d5843aa0a11edc6fa4f1f
Eric Larson <eric@ionrock.org>
2014-10-25 18:25:19 -0500

    Refactor proc to create an environment in a temporary directory.

--- dad/worker/proc.py
+++ dad/worker/proc.py
@@ -1,28 +1,63 @@
 import os
+import sys
 import json
 import shutil
 import tempfile
 import signal
 
 from subprocess import Popen, call
+from collections import namedtuple
 
 import click
 import daemon
 import requests
 
+from dad.worker import app
+
+
+ProcessEnv = namedtuple('ProcessEnv', [
+    'spec',
+    'directory',
+    'logfile'
+])
+
+
+def create_env(spec):
+    tempdir = tempfile.mkdtemp()
+    spec_filename = os.path.join(tempdir, 'spec.json')
+
+    with open(spec_filename, 'w+') as fh:
+        json.dump(spec, fh)
+
+    return ProcessEnv(
+        spec_filename, tempdir, os.path.join(tempdir, 'output.log')
+    )
+
 
 class ChildProcess(object):
-    daemon = True
-
     def __init__(self, spec):
         self.spec = spec
         self._info = {}
 
-    def run(self):
-        with tempfile.NamedTemporaryFile(delete=False) as fh:
-            json.dump(self.spec, fh)
-            self.proc = Popen(['dad-runner', fh.name])
-            self._info = {'pid': self.proc.pid}
+    def run(self, foreground=False):
+        env = create_env(self.spec)
+
+        cmd = [
+            'dad-runner', env.spec,
+            '--working-directory', env.directory,
+            '--cleanup-working-dir'
+        ]
+
+        if foreground:
+            cmd.append('--foreground')
+
+        self.proc = Popen(cmd)
+
+        self._info = {
+            'spec': env.spec,
+            'directory': env.directory,
+            'pid': self.proc.pid,
+        }
 
     def info(self):
         return self._info
@@ -30,9 +65,9 @@ class ChildProcess(object):
 
 class WorkerProcess(object):
 
-    def __init__(self, spec):
+    def __init__(self, spec, sess=None):
         self.spec = spec
-        self.sess = requests.Session()
+        self.sess = sess or requests.Session()
         # TODO: Add auth from the global config
 
     def setup(self):
@@ -56,15 +91,21 @@ class PythonWorkerProcess(WorkerProcess):
     def create_virtualenv(self):
         call(['virtualenv', 'venv'])
 
+    def install_python_deps(self):
+        self.create_virtualenv()
+
+        for dep in self.spec['python_deps']:
+            cmd = [
+                'venv/bin/pip', 'install', dep
+            ]
+
+            if app.config.get('PYTHON_WORKER_CHEESESHOP'):
+                cmd.append('-i %s' % app.config['PYTHON_WORKER_CHEESESHOP'])
+            call()
+
     def setup(self):
         if 'python_deps' in self.spec:
-            self.create_virtualenv()
-
-            for dep in self.spec['python_deps']:
-                call([
-                    'venv/bin/pip', 'install',
-                    '-i', 'http://cheese.yougov.net', dep
-                ])
+            self.install_python_deps()
         self.download_files()
 
     def start(self):
@@ -87,36 +128,64 @@ class PythonWorkerProcess(WorkerProcess):
         print('Cleaning up: %s' % list(args))
         self.proc.terminate()
 
+    def finish(self):
+        print('notifying master...')
+
 
 @click.command()
-@click.argument('proc_config', type=click.File())
-def runner(proc_config):
-    spec = json.load(proc_config)
+@click.argument('specfile', type=click.File())
+@click.option('--cleanup-working-dir', is_flag=True)
+@click.option('--foreground', is_flag=True)
+@click.option('--working-dir', '-w')
+def runner(specfile, cleanup_working_dir, foreground, working_dir=None):
+
+    # Load our spec file
+    spec = json.load(specfile)
     click.echo('Got config: %s' % spec)
 
-    tempdir = tempfile.mkdtemp()
-    click.echo('Working in: %s' % tempdir)
-    logfile = os.path.join(tempdir, 'output.log')
-    click.echo('Logfile: %s' % logfile)
+    # Create a directory to work under and specify our log file.
+    if not working_dir:
+        env = create_env(spec)
+        working_dir = env.directory
+        logfile_name = env.logfile
+        click.echo('Created Working Directory: %s' % env.directory)
+    else:
+        logfile_name = os.path.join(working_dir, 'output.log')
 
-    output_log = open(logfile, 'w+')
+    # Open a file handle for logging
+    if foreground:
+        # If we are not daemonizing, just use stdout
+        output_log = sys.stdout
+    else:
+        click.echo('Logging to: %s' % logfile_name)
+        output_log = open(logfile_name, 'w+')
 
-    # TODO: Only python is supported atm
+    # NOTE: Only python is supported atm
     worker = PythonWorkerProcess(spec)
 
-    context = daemon.DaemonContext(
+    # Redirect stdout/err to our logfile and set our working directory
+    daemon_conf = dict(
         stdout=output_log,
         stderr=output_log,
-        working_directory=tempdir,
+        working_directory=working_dir,
     )
 
+    if foreground:
+        click.echo('Running in the foreground')
+        daemon_conf['detach_process'] = False
+
+    context = daemon.DaemonContext(**daemon_conf)
+
+    # Make sure we give our worker a chance to do clean up and notify
+    # the master on any errors.
     context.signal_map.update({
         signal.SIGTERM: worker.cleanup,
     })
 
-    # TODO: Redirect output and handle errors
     with context:
         worker.setup()
         worker.start()
-        if not worker.code:
-            shutil.rmtree(tempdir)
+        worker.finish()
+        if cleanup_working_dir:
+            click.echo('Cleaning up the working directory: %s' % working_dir)
+            shutil.rmtree(working_dir)
