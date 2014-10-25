f0157d9523f09d6a1392685f7b13cfac9f6bf6c0
Eric Larson <eric@ionrock.org>
2014-10-25 18:24:06 -0500

    Allow overriding the config with app settings yaml in the worker process.

--- dad/worker/__init__.py
+++ dad/worker/__init__.py
@@ -1,8 +1,16 @@
+import os
+
+import yaml
 import click
 
 from flask import Flask, current_app
 app = Flask(__name__)
 app.config.from_object('dad.worker.settings')
+
+if os.environ.get('APP_SETTINGS_YAML'):
+    config = yaml.safe_load(open(os.environ['APP_SETTINGS_YAML']))
+    app.config.update(config)
+
 
 import dad.worker.server  # noqa
 
