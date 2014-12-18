ed8f53994beacb97305c4380bea9f155ae05f04e
Eric Larson <eric@ionrock.org>
2014-12-17 22:01:14 -0600

    Needed a secret key for flask-admin.

--- dadd/master/settings.py
+++ dadd/master/settings.py
@@ -2,5 +2,6 @@ import os
 
 curdir = os.getcwd()
 
+SECRET_KEY = 'something secret'
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dadapp:uhpqZ5pc@localhost/daddb'
 STORAGE_DIR = os.path.join(curdir, 'temp_file_storage')
