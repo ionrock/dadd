e3e3959da9cf726dfb5d6ebb11f8b576a24cc6fd
Eric Larson <eric@ionrock.org>
2014-12-18 21:48:19 -0600

    Change the default db name to dadd_db

--- dadd/master/settings.py
+++ dadd/master/settings.py
@@ -3,5 +3,5 @@ import os
 curdir = os.getcwd()
 
 SECRET_KEY = 'something secret'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dadapp:uhpqZ5pc@localhost/daddb'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dadapp:uhpqZ5pc@localhost/dadd_db'
 STORAGE_DIR = os.path.join(curdir, 'temp_file_storage')
