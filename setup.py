07e5c6be071fe63e1842c5fe497bee63166ad194
Eric Larson <eric@ionrock.org>
2014-12-19 21:03:45 -0600

    Bump version: 0.1.10 → 0.1.11

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ requirements = [
 
 setup(
     name='dadd',
-    version='0.1.10',
+    version='0.1.11',
     description=('Dadd is a remote worker hosts for '
                  'starting and forgetting processes.'),
     long_description=readme + '\n\n' + history,
