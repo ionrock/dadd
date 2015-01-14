64ead7dd2923a3ec7800b7640c12bdb05a2d8345
Eric Larson <eric@ionrock.org>
2015-01-13 22:41:42 -0600

    Getting closer to watching the logs in the worker process as jobs are running.

--- dadd/worker/logging.py
+++ dadd/worker/logging.py
@@ -1,5 +1,8 @@
 import os
+import time
 import threading
+
+from dadd.worker import app
 
 
 class LogWatcher(threading.Thread):
@@ -8,9 +11,27 @@ class LogWatcher(threading.Thread):
         super(LogWatcher, self).__init__()
         self.fname = fname
 
+    def write(self, chunk):
+        app.logger.info(chunk)
+
+    def running(self):
+        try:
+            os.kill(self.pid, 0)
+            app.logger.info('%s is up' % self.pid)
+            return True
+        except OSError:
+            app.logger.info('%s is down' % self.pid)
+            return False
+
+    def tail(self, fh):
+        self.write('Tailing: %s' % fh.name)
+        curr_position = fh.tell()
+        line = fh.readline()
+        if not line:
+            fh.seek(curr_position)
+        else:
+            self.write(line)
+
     def run(self):
         if os.path.exists(self.fname):
-            for line in open(self.fname):
-                print(line)
-        else:
-            print('%s does not exist' % self.fname)
+            return self.tail(open(self.fname))
