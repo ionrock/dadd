0a4a0faa014cf4f011489f39dd305121fa958a32
Eric Larson <eric@ionrock.org>
2015-01-15 10:07:35 -0600

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requirements = [
 
 setup(
     name='dadd',
-    version='0.2.1',
+    version='0.2.2',
     description='Dadd administers daemons.',
     long_description=readme + '\n\n' + history,
     author='Eric Larson',
