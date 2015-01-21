20997d1b90295ee3b636de56815ca878235089ba
Eric Larson <eric@ionrock.org>
2015-01-21 11:03:15 -0600

    Make the virtualenv optional.
    If we are running in a container using the "system" virtualenv is
    OK. This will need some more precautions and configuration, but for now,
    we'll let it be somewhat automatic.

--- dadd/worker/processes/python.py
+++ dadd/worker/processes/python.py
@@ -9,18 +9,20 @@ class PythonWorkerProcess(WorkerProcess):
 
     def create_virtualenv(self):
         call(['virtualenv', 'venv'])
+        os.environ['PATH'] = os.path.abspath('venv') + ':' + os.environ['PATH']
 
     def install_python_deps(self):
-        self.create_virtualenv()
+        try:
+            self.create_virtualenv()
+        except:
+            pass
 
         # Make sure we have a list
         if isinstance(self.spec['python_deps'], basestring):
             self.spec['python_deps'] = [self.spec['python_deps']]
 
         for dep in self.spec['python_deps']:
-            cmd = [
-                'venv/bin/pip', 'install',
-            ]
+            cmd = ['pip', 'install']
 
             if self.spec.get('python_cheeseshop'):
                 cmd.extend(['-i', self.spec['python_cheeseshop']])
@@ -30,11 +32,6 @@ class PythonWorkerProcess(WorkerProcess):
             call(cmd, stderr=STDOUT, stdout=self.output)
 
     def setup(self):
+        super(PythonWorkerProcess, self).setup()
         if 'python_deps' in self.spec:
             self.install_python_deps()
-        self.download_files()
-
-    def start(self):
-        # Add our virtualenv to the path
-        os.environ['PATH'] += ':%s' % os.path.abspath('./venv/bin/')
-        super(PythonWorkerProcess, self).start()
