ca94513b3487232a2f9714ddc129d141c011b4af
Eric Larson <eric@ionrock.org>
2015-01-23 16:38:28 -0600

    Sort the logfile by added time.

--- dadd/master/admin.py
+++ dadd/master/admin.py
@@ -12,9 +12,19 @@ class ProcessModelView(ModelView):
         super(ProcessModelView, self).__init__(models.Process, session)
 
 
+class LogfileModelView(ModelView):
+    # Make the latest first
+    column_default_sort = ('added_time', True)
+
+    def __init__(self, session):
+        super(LogfileModelView, self).__init__(models.Logfile, session)
+
+
 def admin(app):
     admin = Admin(app)
     session = models.db.session
+
     admin.add_view(ProcessModelView(session))
+    admin.add_view(LogfileModelView(session))
+
     admin.add_view(ModelView(models.Host, session))
-    admin.add_view(ModelView(models.Logfile, session))
