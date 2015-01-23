2548ecd64d6a26b09fe79f5a369f731c66410aa0
Eric Larson <eric@ionrock.org>
2015-01-23 15:54:37 -0600

    Sort the processes by start time by default in the Admin.

--- dadd/master/admin.py
+++ dadd/master/admin.py
@@ -4,9 +4,17 @@ from flask.ext.admin.contrib.sqla import ModelView
 from dadd.master import models
 
 
+class ProcessModelView(ModelView):
+    # Make the latest first
+    column_default_sort = ('start_time', True)
+
+    def __init__(self, session):
+        super(ProcessModelView, self).__init__(models.Process, session)
+
+
 def admin(app):
     admin = Admin(app)
     session = models.db.session
-    admin.add_view(ModelView(models.Process, session))
+    admin.add_view(ProcessModelView(session))
     admin.add_view(ModelView(models.Host, session))
     admin.add_view(ModelView(models.Logfile, session))
